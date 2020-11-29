907c2cd2639476475734bc597cd0bafb35e8aa2e
Cajetan <cajy78@live.com>
2020-11-29 20:29:12 +0530

    initial commit

---
+++ src/test/java/cases/Baseclass.java
@@ -0,0 +1,22 @@
+package cases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class Baseclass {
+	
+	public WebDriver driver;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/cajetanfernandes/Downloads/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.redbus.in");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);	  
+	}
+}
